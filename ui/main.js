fb883d0b05b195aa5ecf36ab416b79bf10c3a378
Alex Nielsen <alex.nielsen@gmail.com>
2017-08-18 15:20:09 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,11 @@
 console.log('Loaded!');
+
+var element = document.getElementById('main-txt');
+
+element.innderHTML = "changed text";
+
+var img = document.getelementById('madi');
+
+img.onclick = function() {
+    img.style.marginLeft = '100px';
+}
