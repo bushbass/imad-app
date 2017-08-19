1d63b518307770b59f32ddaf5e836e1322c9da37
Alex Nielsen <alex.nielsen@gmail.com>
2017-08-19 14:08:21 -0400

    [imad-console] Updates ui/article.js

--- ui/article.js
+++ ui/article.js
@@ -46,7 +46,7 @@ submit.onclick = function(){
                    list += `<li>${names[i]} </li>`;
                 }
                 var ul = document.getElementById('namelist');
-                console.log(list);
+                
                 ul.innerHTML = list;
                 }
             }
