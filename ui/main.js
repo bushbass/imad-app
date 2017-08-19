a7d72947adcb9c85deae572786f832c94475793e
Alex Nielsen <alex.nielsen@gmail.com>
2017-08-19 13:55:08 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,24 +1,23 @@
 // Counter code
-var button = document.getElementById('counter');
-var counter = 0;
-button.onclick = function () {
-    
-    //create a request object
-    var request = new XMLHttpRequest();
-        //capture response and store it in a variable
-        request.onreadystatechange = function () {
-        if (request.readyState === XMLHttpRequest.DONE){
-            //take some action
-            if (request.status === 200) {
-                var counter = request.responseText;
-                var span = document.getElementById('count');
-                span.innerHTML = counter.toString();
-            }
-        }
-    };
-    request.open('GET', 'http://alexnielsen.imad.hasura-app.io/counter',true);
-    request.send(null);
-};
+// var button = document.getElementById('counter');
+// var counter = 0;
+// button.onclick = function () {
+//         //create a request object
+//     var request = new XMLHttpRequest();
+//         //capture response and store it in a variable
+//         request.onreadystatechange = function () {
+//         if (request.readyState === XMLHttpRequest.DONE){
+//             //take some action
+//             if (request.status === 200) {
+//                 var counter = request.responseText;
+//                 var span = document.getElementById('count');
+//                 span.innerHTML = counter.toString();
+//             }
+//         }
+//     };
+//     request.open('GET', 'http://alexnielsen.imad.hasura-app.io/counter',true);
+//     request.send(null);
+// };
 
 // submit name
 
