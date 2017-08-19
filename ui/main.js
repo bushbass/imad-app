0edee869448febb400107ebd6f7bb35f29feea82
Alex Nielsen <alex.nielsen@gmail.com>
2017-08-19 13:34:36 -0400

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -58,3 +58,35 @@ submit.onclick = function(){
     request.send(null);
 }; 
 
+
+var comment = document.getElementById('comment_btn');
+comment.onclick = function(){
+  
+   //create a request object
+    var request = new XMLHttpRequest();
+    
+    //capture response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE){
+            //take some action
+            if (request.status === 200) {
+ 
+                //capture list of comments and render as a list
+                var comments = request.responseText;
+                comments = JSON.parse(comments);
+                var list = '';
+                for (var i=0; i < comments.length; i++) {
+                   list += `<li>${comments[i]} </li>`;
+                }
+                var ul = document.getElementById('commentlist');
+                console.log(list);
+                ul.innerHTML = list;
+                }
+            }
+        };
+        var commentInput = document.getElementById('comment');
+
+    var comment = commentInput.value;
+    request.open('GET', 'http://alexnielsen.imad.hasura-app.io/submit-comment?comment=' + comment,true);
+    request.send(null);
+}; 
